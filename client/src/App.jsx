5c30ea16d79fdf913567efa5bf377955fa50fa27
Agron Gojcaj <agrongojcaj6@gmail.com>
2025-06-13 01:38:52 -0400

    Made /dashboard enpoint protected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import DashboardPage from './pages/DashboardPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -11,6 +12,14 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/dashboard" element = {<DashboardPage />} />
+      <Route 
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <DashboardPage />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
