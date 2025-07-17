f1185da5d30e5b3cfa0f6ab8016bf8dd350f0a94
Agron Gojcaj <agrongojcaj6@gmail.com>
2025-07-17 19:52:48 -0400

    Implemented updateUserProfile backend functionality

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -54,3 +54,23 @@ exports.getUserProfile = async (req, res) => {
     res.status(404).json({ message: 'User not found' });
   }
 };
+
+exports.updateUserProfile = async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      username: updatedUser.username,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404).json({ message: 'User not found' });
+  }
+};
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, authUser, getUserProfile } = require('../controllers/authController');
+const { registerUser, authUser, getUserProfile, updateUserProfile } = require('../controllers/authController');
 const protect = require('../middleware/authMiddleware');
 const User = require('../models/User');
 
@@ -8,5 +8,6 @@ router.post('/register', registerUser);
 router.post('/login', authUser);
 
 router.get('/profile', protect, getUserProfile);
+router.put('/profile', protect, updateUserProfile);
 
 module.exports = router;
